6a7bb80f1dc1492c6de5bec76c8d5441b0d8d171
NikitaButorov <butorov.nikita@list.ru>
2024-05-28 09:29:13 +0300

    NE-2 <message>

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, () => {
+    console.log(`Server in running on port ${PORT} `)
+})
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'views', 'index.html'))
+// })
+
