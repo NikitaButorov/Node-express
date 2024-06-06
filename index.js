dac2f236859acf18e31db4b790562362b2569fcd
NikitaButorov <butorov.nikita@list.ru>
2024-06-06 08:50:05 +0300

    Add navigation

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-const express = require('express')
-const path = require('path')
-const exphbs = require('express-handlebars')
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
-const app = express()
+const app = express();
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs'
-})
+    defaultLayout: 'main', 
+    extname: 'hbs' 
+});
 
 app.engine('hbs', hbs.engine)
 app.set('view engine','hbs')
 app.set('views','views')
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index')
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
 
-
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server in running on port ${PORT} `)
-})
+    console.log('listening on port ${PORT}');});
